c0a97d16477be695a1fe288da2bf27ff45c3ac44
Jeffrey Zhang <zhang.lei.fly@gmail.com>
2021-06-27 13:44:28 +0800

    add home page

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     author='Jeffrey Zhang',
     author_email='zhang.lei.fly@gmail.com',
     url='https://www.github.com/jeffrey4l/docker-recreate',
+    home_page='https://www.github.com/jeffrey4l/docker-recreate',
     entry_points={
         'console_scripts': [
             'docker-recreate=main:main'
