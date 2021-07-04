f46f3150ec7c1be9f032caba63359227e56c96a1
Jeffrey Zhang <zhang.lei.fly@gmail.com>
2021-07-04 12:12:08 +0800

    use git to get proper version

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 from setuptools import setup
+import subprocess
+
+
+def get_version():
+    output = subprocess.check_output(['git', 'describe', '--tags'])
+    output = output.decode('utf8')
+    return output.strip()
 
 
 setup(
     name='docker-recreate',
+    version=get_version(),
     description='Get docker run command from container',
     author='Jeffrey Zhang',
     author_email='zhang.lei.fly@gmail.com',
@@ -15,5 +23,4 @@ setup(
             'docker-recreate=main:main'
         ]},
     py_modules=['main'],
-    pbr=True
     )
